106c4c7f1c8db511d556323b509a134debabeb69
DinuEugen <dinueugenmarin@gmail.com>
2024-10-15 16:46:05 +0200

    second commit

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -2,18 +2,18 @@
 @section('content')
 
 <div class="blog-wrapper">
-<div class="show-container">
+  <div class="show-container">
     <div class="left-content"><img src="{{ asset('images/'.$blog->image) }}" alt=""></div>
     <div class="right-content">
         <h1>{{ $blog->title }}</h1>
         <p>{{ $blog->description }}</p>
         <p>{{ $blog->content }}</p>
         <p>Posted on {{ $blog->created_at }}</p>
-<div class="button"><a href="{{ route('blog.index') }}">Go to all blogs</a></div>
+         <div class="button"><a href="{{ route('blog.index') }}">Go to all blogs</a></div>
     </div>
 
 
-</div>
+   </div>
 </div>
 @endsection
 
